c0a2533455ae19d076c26ecb2546fd9b8355b8a0
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-20 11:31:58 -0700

    component folder; TodoForm skeleton; need to build state

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,20 @@
+import React, {useState, useReducer} from 'react';
+
+const TodoForm = () => {
+    return (
+        <div>
+            <h1>Lambda Todo's</h1>
+            <label htmlFor="new-todo">
+                <input
+                    type="text"
+                    name="todo"
+                    placeholder="New Todo Task..."
+                />
+            </label>
+            <button>Add</button>
+            <button>Clear Completed</button>
+        </div>
+    )
+};
+
+export default TodoForm;
