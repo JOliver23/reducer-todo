b0bb279ecabf6275835d30c301a3832acd618219
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-20 12:33:03 -0700

    reducer built

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,26 @@
+export const initialState = {
+    task: "Sample Task from reducer",
+    note: "we have a todo",
+    complete: false
+};
+
+export const taskReducer = (state, action) => {
+    if (action.type === "ADD_TASK") {
+        return {
+            ...state,
+            task: action.payload 
+        };
+    } else if (action.type === "ADD_NOTE") {
+        return {
+            ...state,
+            note: action.payload
+        };
+    } else if (action.type === "TOGGLE_COMPLETE") {
+        return {
+            ...state,
+            complete: !state.complete
+        };
+    }
+    return state;
+}
+
