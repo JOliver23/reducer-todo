300da112f75f2ccd0f0aeae597b0c3a6a7be41fe
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-21 12:59:08 -0700

    restart; build form in app.js; onto the reducer

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import TodoForm from './components/TodoForm';
+import React, { useState, useReducer} from 'react';
+import {initialState, taskReducer} from '../reducers';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [task, dispatch] = useReducer(taskReducer, initialState);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChanges = e => {
+    setNewTask(e.target.value)
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setNewTask("")
+  };
+
   return (
     <div className="App">
-      <TodoForm />
+      <h1>Lambda's Todo List</h1>
+      <form>
+        <input type="text" name="task" placeholder="New Todo Task..." value={newTask} onChange={handleChanges} />      
+        <button onClick={() => dispatch({type: "ADD_TASK"})}>Add Todo</button>
+        <button>Clear Complete</button>
+      </form>
       <TodoList />
     </div>
   );
