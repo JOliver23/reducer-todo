3dc42ebf6bf8f2c2071e1de420e247ca222746ad
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-21 13:06:58 -0700

    comment out old; refator reucer

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,32 +1,48 @@
-export const initialState = {
-    todos: [{
-        task: "Sample Task from reducer",
-        complete: false,
-        id: 666
-    }]
-};
+export const initialState = [{
+    task: "practice reducer",
+    complete: false,
+    id:666
+}];
 
 export const taskReducer = (state, action) => {
     console.log("state: ", state.todos);
     console.log("action.payload: ", action.payload);
-    if (action.type === "ADD_TASK") {
-        const newTodo = {
-            task: action.payload,
-            complete:false,
-            id: Date.now()
-        }
-        console.log("newToDo in taskReducer index.js: ", newTodo);
+    switch (action.type) {
+        case 'ADD_TASK':
+            return [
+                ...state,
+                {
+                    task: action.payload,
+                    complete: false,
+                    id: new Date()
+                }
+            ];
+        case 'TOGGLE_COMPLETE':
+            return state.map(newTask =>
+                newTask.id === action.payload.id ? {...newTask, complete: !newTask.complete} : newTask);
+        case 'CLEAR_COMPLETE':
+            return state.filter(task =>
+                !task.complete);
+        default: return state;
+    };
+    // if (action.type === "ADD_TASK") {
+    //     const newTodo = {
+    //         task: action.payload,
+    //         complete:false,
+    //         id: Date.now()
+    //     }
+    //     console.log("newToDo in taskReducer index.js: ", newTodo);
 
-        return {
-            ...state,
-            todos: [...state.todos, newTodo]
-        };
-    } else if (action.type === "TOGGLE_COMPLETE") {
-        return {
-            ...state,
-            complete: !state.complete
-        };
-    } 
-    return state;
+    //     return {
+    //         ...state,
+    //         todos: [...state.todos, newTodo]
+    //     };
+    // } else if (action.type === "TOGGLE_COMPLETE") {
+    //     return {
+    //         ...state,
+    //         complete: !state.complete
+    //     };
+    // } 
+    // return state;
 }
 
